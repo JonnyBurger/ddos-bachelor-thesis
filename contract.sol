d099c042f49b4e882ad8410c3aa2830493ad1029
Jonny Burger <jonathanburger11@gmail.com>
2017-03-15 11:00:24 +0100

    Initial commit

---
+++ contract.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.4.8;
+
+contract DdosMitigation {
+    struct Report {
+        address reporter;
+        string url;
+    }
+
+    address public owner;
+    Report[] public reports;
+
+    function DdosMitigation() {
+        owner = msg.sender;
+    }
+
+    function report(string url) {
+        reports.push(Report({
+            reporter: msg.sender,
+            url: url
+        }));
+    }
+}
