b852449f665fe6ffb6f09aacac58a1b6791da0e2
Jonny Burger <jonathanburger11@gmail.com>
2017-03-20 17:28:41 +0100

    fix cli

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const meow = require('meow');
 const deployContract = require('./lib/deploy-contract');
 const estimateGas = require('./lib/estimate-gas');
 const contracts = require('./lib/get-contracts');
+const getProvider = require('./lib/get-provider');
 
 const cli = meow(`
     Commands:
@@ -9,12 +10,14 @@ const cli = meow(`
         deploy-contract [contract]
 `);
 
+const {web3} = getProvider();
+
 (() => {
     switch (cli.input[0]) {
         case 'estimate-gas':
-            return estimateGas(contracts[cli.input[1]]);
+            return estimateGas(web3, contracts[cli.input[1]]);
         case 'deploy-contract':
-            return deployContract(contracts[cli.input[1]]);
+            return deployContract(web3, contracts[cli.input[1]]);
         default:
             return Promise.resolve(cli.help);
     }
